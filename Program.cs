a25857a445fa75ff618143d82d2bd91a7a5c86d6
syunnjack <syunnjack@gmail.com>
2026-08-03 12:02:49 +0900

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+Exit code: 0
+Wall time: 0.8 seconds
+Output:
+namespace BarcodeWorkInfoComplete;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
+
